036a2b17199d2d225571949ee0e37a3e271cdaa1
Robert Ball <jhrobert@pm.me>
2025-06-16 23:08:09 -0700

    updated nvim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+local set = vim.keymap.set
+
+-- vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+-- vim.keymap.set("i", "jj", "<Esc>", { noremap = false, silent = true })
+set("n", "gh", "^", { desc = "Go to first non-whitespace character", noremap = false })
+set("n", "gl", "$", { desc = "Go to end of line", noremap = false })
+set("n", "<leader>xr", "<cmd>source %<CR>", { desc = "Execute the current file" })
--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -5,16 +5,4 @@ return {
       colorscheme = "catppuccin-macchiato",
     },
   },
-  {
-    "catppuccin/nvim",
-    specs = {
-      "akinsho/bufferline.nvim",
-      optional = true,
-      opts = function(_, opts)
-        if (vim.g.colors_name or ""):find("catppuccin") then
-          opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-        end
-      end,
-    },
-  },
 }
