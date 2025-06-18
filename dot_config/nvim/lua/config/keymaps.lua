b550478c1b7c10cdb2d360e6d396f984bf2ff35c
Robert Ball <jhrobert@pm.me>
2025-06-17 22:16:45 -0700

    updated nvim

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -4,8 +4,7 @@
 
 local set = vim.keymap.set
 
--- vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
--- vim.keymap.set("i", "jj", "<Esc>", { noremap = false, silent = true })
-set("n", "gh", "^", { desc = "Go to first non-whitespace character", noremap = false })
-set("n", "gl", "$", { desc = "Go to end of line", noremap = false })
+set("i", "jj", "<Esc>", { noremap = false, silent = true })
+set("n", "gh", "^", { desc = "Go to first non-whitespace character", noremap = true })
+set("n", "gl", "$", { desc = "Go to end of line", noremap = true })
 set("n", "<leader>xr", "<cmd>source %<CR>", { desc = "Execute the current file" })
