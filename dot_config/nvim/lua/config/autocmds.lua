42aa91ac20edb3603e4a6ceaa089780d3705faba
Robert Ball <jhrobert@pm.me>
2025-12-10 11:20:50 -0800

    various updates

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.bu", "*.butane" },
+  command = "setlocal filetype=yaml",
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.ign",
+  command = "setlocal filetype=json",
+})
--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,7 @@ set("i", "jj", "<Esc>", { noremap = false, silent = true })
 set("n", "gh", "^", { desc = "Go to first non-whitespace character", noremap = true })
 set("n", "gl", "$", { desc = "Go to end of line", noremap = true })
 set("n", "<leader>xr", "<cmd>source %<CR>", { desc = "Execute the current file" })
+set("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Exit terminal mode", noremap = true, silent = true })
+set({ "i", "x", "n", "s" }, "<D-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+set("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Hover" })
